a8c1610411a26e80486db396b68a437d01955d99
tewpongamorn123 <tewpongamorn@gmail.com>
2024-03-26 03:57:52 +0700

    GG

--- models/Joboffer.js
+++ models/Joboffer.js
@@ -4,7 +4,7 @@ const JobofferSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please add a name'],
-        unique: true,
+        unique: false,
         trim: true,
         maxlength: [50, 'Name cannot be more than 50 characters']
     },
